864d7d04b8557b2dee33f728596322d92cf14c78
Montalvas <62609007+montalvas@users.noreply.github.com>
2020-07-30 21:32:13 -0300

    Update
    Corrections

--- script.js
+++ script.js
@@ -3,15 +3,22 @@ var botao = document.getElementById("action");
 botao.addEventListener("click", clicar);
 
 function clicar(){
-    var num = Number((document.getElementById("num")).value);
-    var res = document.querySelector("div.res");
-    res.innerHTML = "";
-    var lista = document.createElement("select");
-
-    for (var c = 1; c <= 10; c++){
-        var mult = document.createElement("option");
-        mult.innerHTML = `${num} X ${c} = ${num * c}`;
-        lista.insertBefore(mult, null);
+    var n = document.getElementById("num");
+    if (n.value.length == 0){
+        window.alert("Por favor, informe um número!");
     }
-    res.appendChild(lista);
+    else{
+        var num = Number(n.value);
+        var res = document.querySelector("div.res");
+        res.innerHTML = "";
+        var lista = document.createElement("select");
+
+        for (var c = 1; c <= 10; c++){
+            var mult = document.createElement("option");
+            mult.innerHTML = `${num} X ${c} = ${num * c}`;
+            lista.insertBefore(mult, null);
+        }
+        lista.size = "10";
+        res.appendChild(lista);
+    }
 }
